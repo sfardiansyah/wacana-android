48166b21fbef55434298804448fb2614a4c11a5d
Syahrul Ardiansyah <syahrul.findi@gmail.com>
2019-11-01 15:02:19 +0700

    Navigate to shopping cart

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/syahrul_findi_ardiansyah/wacana/ui/bookDetail/BookDetailFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/syahrul_findi_ardiansyah/wacana/ui/bookDetail/BookDetailFragment.kt
@@ -11,9 +11,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import id.ac.ui.cs.mobileprogramming.syahrul_findi_ardiansyah.wacana.R
 import id.ac.ui.cs.mobileprogramming.syahrul_findi_ardiansyah.wacana.databinding.FragmentBookDetailBinding
+import id.ac.ui.cs.mobileprogramming.syahrul_findi_ardiansyah.wacana.model.Book
 import id.ac.ui.cs.mobileprogramming.syahrul_findi_ardiansyah.wacana.utilities.InjectorUtils
 
 class BookDetailFragment: Fragment() {
@@ -39,11 +41,29 @@ class BookDetailFragment: Fragment() {
         bookDetailViewModel.book.observe(this, Observer {
             it?.let {
                 binding.book = it
+                binding.clickListener = AddToCartListener {
+                    bookDetailViewModel.onCTAClicked(it)
+                }
             }
         })
+
+        setupNavigation()
 
         binding.lifecycleOwner = this
 
         return binding.root
     }
+
+    private fun setupNavigation() {
+        bookDetailViewModel.navigateToBookDetail.observe(this, Observer { book ->
+            book?.let {
+                this.findNavController().navigate(BookDetailFragmentDirections.actionBookDetailFragmentToNavigationNotifications(book.id))
+                bookDetailViewModel.doneNavigating()
+            }
+        })
+    }
+}
+
+class AddToCartListener(val clickListener: (book: Book) -> Unit) {
+    fun onClick(book: Book) = clickListener(book)
 }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/syahrul_findi_ardiansyah/wacana/ui/bookDetail/BookDetailViewModel.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/syahrul_findi_ardiansyah/wacana/ui/bookDetail/BookDetailViewModel.kt
@@ -1,9 +1,24 @@
 package id.ac.ui.cs.mobileprogramming.syahrul_findi_ardiansyah.wacana.ui.bookDetail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import id.ac.ui.cs.mobileprogramming.syahrul_findi_ardiansyah.wacana.model.Book
 import id.ac.ui.cs.mobileprogramming.syahrul_findi_ardiansyah.wacana.repository.BookRepository
 
 class BookDetailViewModel(bookRepository: BookRepository, private val bookId: Int): ViewModel() {
 
+    private val _navigateToShoppingCart = MutableLiveData<Book>()
+
     val book = bookRepository.getBook(bookId)
+
+    val navigateToBookDetail: LiveData<Book> = _navigateToShoppingCart
+
+    fun onCTAClicked(book: Book) {
+        _navigateToShoppingCart.value = book
+    }
+
+    fun doneNavigating() {
+        _navigateToShoppingCart.value = null
+    }
 }
